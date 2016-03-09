4ab7cbe98520e5272070806439d6c0375e920a37
Jeffrey Cimmino <jeffrey.cimmino@quinnipiac.edu>
2016-03-09 00:19:24 -0500

    Added whoAreYou.scala

---
+++ Mimir/whoAreYou.scala
@@ -0,0 +1,5 @@
+println("Hello, what is your first name?");
+val fName = readLine();
+println("Thank you. What is your last name?");
+val lName = readLine();
+println("Nice to meet you, " + fName + " " + lName + ".");
