159e473350af8a610611c33e39d35804715de979
Jeffrey Cimmino <jeffrey.cimmino@quinnipiac.edu>
2016-03-09 02:12:36 -0500

    Added max.scala

---
+++ Mimir/max.scala
@@ -0,0 +1,8 @@
+println("Please enter four numbers.");
+val w = readInt();
+val x = readInt();
+val y = readInt();
+val z = readInt();
+val wxMax = w.max(x);
+println("The maximum of the four numbers is " + ((w.max(x)).max(y)).max(z) + ".");
+
