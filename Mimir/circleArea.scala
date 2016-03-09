ca838f3dfd3524d3c7204b28ae1af8f18a1239cc
Jeffrey Cimmino <jeffrey.cimmino@quinnipiac.edu>
2016-03-09 03:05:59 -0500

    Fixed circleArea.scala

--- Mimir/circleArea.scala
+++ Mimir/circleArea.scala
@@ -1,4 +1,6 @@
 println("What is the radius of the circle in centimeters?");
 val r: Double = readLine().toDouble;
 val pi = scala.math.Pi;
-println("The area of a circle of radius " + r + " cm is " + ((r * r)*pi).toInt + " cm^2.");
+val x = ((r * r) * pi);
+val xFinal = (scala.math.BigDecimal(x).setScale(0, BigDecimal.RoundingMode.HALF_UP).toDouble).toInt;
+println("The area of a circle of radius " + r + " cm is " + xFinal + " cm^2.");
